068e0b9806ba63b176ee4414ba31ac1fd69dda78
tarekfb <56593098+tarekfb@users.noreply.github.com>
2021-11-04 15:28:55 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'next'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
